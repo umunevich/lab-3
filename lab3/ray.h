5f5fa95caaa82f7b3f957973338e76d22db0f256
Yana <125117368+umunevich@users.noreply.github.com>
2024-03-22 22:22:08 +0200

    Delete lab3 directory

--- lab3/ray.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-namespace geometry
-{
-	namespace ray
-	{
-		bool read(double& x1, double& y1, double& x2, double& y2);
-		void readRay(double& x1, double& y1, double& x2, double& y2);
-	} // namespace ray
-} //namespace geometry
