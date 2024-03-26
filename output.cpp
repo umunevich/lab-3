35e7ca80172540315bed64ece878ddcb894d0dd7
Yana <125117368+umunevich@users.noreply.github.com>
2024-03-26 18:48:11 +0200

    Add files via upload

---
+++ output.cpp
@@ -0,0 +1,22 @@
+#include "output.h"
+#include<iostream>
+
+
+namespace output 
+{
+	void aboutMe()
+	{
+		std::cout << "Utochkina Yana K-13\nLab 3 'Geometry'" << std::endl;
+	}
+
+	void taskOfProgram()
+	{
+		std::cout << "3. Does the ray intersect the triangle?" << std::endl;
+	}
+
+	void bye()
+	{
+		std::cout << "Bye)" << std::endl;
+	}
+
+} // namespace output
---
+++ ray.h
@@ -0,0 +1,9 @@
+#pragma once
+namespace geometry
+{
+	namespace ray
+	{
+		bool read(double& x1, double& y1, double& x2, double& y2);
+		void readRay(double& x1, double& y1, double& x2, double& y2);
+	} // namespace ray
+} //namespace geometry
